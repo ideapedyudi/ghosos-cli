762e7b46adffa29838bf60bc05484041ff3e0d6f
ideapedyudi <ideapedyudi@gmail.com>
2026-04-27 21:52:24 +0700

    Expand platform list to 18 social media platforms

--- src/data/platforms.js
+++ src/data/platforms.js
@@ -31,5 +31,66 @@ export const platforms = [
     name: 'Facebook',
     url: 'https://www.facebook.com/{}',
     validationType: 'status'
+  },
+  {
+    name: 'YouTube',
+    url: 'https://www.youtube.com/@{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Reddit',
+    url: 'https://www.reddit.com/user/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Twitch',
+    url: 'https://www.twitch.tv/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'SoundCloud',
+    url: 'https://soundcloud.com/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Behance',
+    url: 'https://www.behance.net/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Dribbble',
+    url: 'https://dribbble.com/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Medium',
+    url: 'https://medium.com/@{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Spotify',
+    url: 'https://open.spotify.com/user/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Steam',
+    url: 'https://steamcommunity.com/id/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Telegram',
+    url: 'https://t.me/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Snapchat',
+    url: 'https://www.snapchat.com/add/{}',
+    validationType: 'status'
+  },
+  {
+    name: 'Linktree',
+    url: 'https://linktr.ee/{}',
+    validationType: 'status'
   }
 ];
+
